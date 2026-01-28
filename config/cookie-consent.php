7aa26aee5160376cb9399779d44dd646009dfbac
Lytrox <9468383+Lytrox@users.noreply.github.com>
2026-01-28 13:47:54 +0100

    Added cookie consent plugin

---
+++ config/cookie-consent.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'css' => 'https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css',
+    'js' => 'https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js',
+    'content' => [
+        'href' => '/privacy-policy',
+        'close' => '&#x274c;',
+    ],
+    'palette' => [
+        'popup' => [
+            'background' => '#696969',
+            'text' => '#FFFFFF',
+            'link' => '#FFFFFF',
+        ],
+        'button' => [
+            'background' => 'transparent',
+            'border' => '#f8e71c',
+            'text' => '#f8e71c',
+        ],
+        'highlight' => [
+            'background' => '#f8e71c',
+            'border' => '#f8e71c',
+            'text' => '#000000',
+        ],
+    ],
+    'position' => 'bottom-right', // top-left, top-right, bottom-left, bottom-right
+    'theme' => 'block', // block, edgeless, classic
+];
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+})->name('privacy-policy');
