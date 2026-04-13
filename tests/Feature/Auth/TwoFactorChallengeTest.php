a345e200c8da201e7bd264882a6bb711dffb3cbd
Lytrox <9468383+Lytrox@users.noreply.github.com>
2026-04-13 19:35:21 +0200

    Add HTTP fake response for Turnstile validation in two-factor authentication tests

--- tests/Feature/Auth/TwoFactorChallengeTest.php
+++ tests/Feature/Auth/TwoFactorChallengeTest.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 use Laravel\Fortify\Features;
 
 beforeEach(function () {
     $this->skipUnlessFortifyFeature(Features::twoFactorAuthentication());
+
+    Http::fake([
+        'challenges.cloudflare.com/*' => Http::response(['success' => true], 200),
+    ]);
 });
 
 test('two factor challenge redirects to login when not authenticated', function () {
@@ -24,5 +29,6 @@ test('two factor challenge can be rendered', function () {
     $this->post(route('login.store'), [
         'email' => $user->email,
         'password' => 'password',
+        'cf-turnstile-response' => 'test-token',
     ])->assertRedirect(route('two-factor.login'));
 });
